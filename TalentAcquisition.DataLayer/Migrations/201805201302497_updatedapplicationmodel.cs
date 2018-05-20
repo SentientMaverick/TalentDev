a3b06b896ae325a5f94fdc2e73c691cc0148290c
SentientMaverick <monglord_13@yahoo.com>
2018-05-20 18:47:56 +0100

    Commit before

---
+++ TalentAcquisition.DataLayer/Migrations/201805201302497_updatedapplicationmodel.cs
@@ -0,0 +1,20 @@
+namespace TalentAcquisition.DataLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedapplicationmodel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Interview", "JobApplicationID");
+            AddForeignKey("dbo.Interview", "JobApplicationID", "dbo.JobApplication", "JobApplicationID", cascadeDelete: false);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Interview", "JobApplicationID", "dbo.JobApplication");
+            DropIndex("dbo.Interview", new[] { "JobApplicationID" });
+        }
+    }
+}
